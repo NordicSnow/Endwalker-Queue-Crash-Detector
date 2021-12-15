500621f1452735dbcfbc4f50407e1feaccddc1a9
Zeke Ross <zeke@zekial.io>
2021-12-14 21:38:48 -0600

    Add files via upload

---
+++ queuestalker.py
@@ -0,0 +1,28 @@
+import os, time, requests
+
+#sets up http connection for telegram
+token = "token"
+chatID = "1234567890"
+filePath = r"C:\Users\zekeross\Documents\My Games\FINAL FANTASY XIV - A Realm Reborn\FFXIV.cfg"
+
+
+
+url = f"https://api.telegram.org/bot{token}"
+
+#gets last modified time from ffxiv config file (epoch time format)
+def getCurrentTime():
+    fileTime = os.path.getmtime(filePath)
+    return fileTime
+
+#infinite loop TODO: make this not terrible
+while True:
+    oldTime = getCurrentTime() #grabs current file time
+    time.sleep(60) #waits a minute
+
+    currTime = getCurrentTime() - oldTime
+    #compares old time with new time
+    if(currTime >= 65): #if its been over a minute since last update, assume game has crashed
+        #sends telegram message to warn user
+        params = {"chat_id": chatID, "text": "Lobby crashed! Log in to save it!"}
+        r = requests.get(url + "/sendMessage", params=params)
+        break
